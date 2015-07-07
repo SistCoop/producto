1d1084fba169a5daff6e452914706f5e27ae3a94
Carlos <carlosthe19916@gmail.com>
2015-07-07 12:59:36 -0500

    1

---
+++ producto-model/producto-model-api/src/main/java/org/sistcoop/producto/models/enums/TipoCuentaPersonal.java
@@ -0,0 +1,20 @@
+package org.sistcoop.producto.models.enums;
+
+public enum TipoCuentaPersonal {
+
+    /**
+     * Cuenta de ahorros
+     */
+    AHORRO,
+
+    /**
+     * Cuenta corriente
+     */
+    CORRIENTE,
+
+    /**
+     * Cuenta a plazo fijo
+     */
+    PLAZO_FIJO
+
+}
--- producto-model/producto-model-jpa/src/main/java/org/sistcoop/producto/models/jpa/entities/ProductoCuentaPersonalEntity.java
+++ producto-model/producto-model-jpa/src/main/java/org/sistcoop/producto/models/jpa/entities/ProductoCuentaPersonalEntity.java
@@ -1,11 +1,14 @@
 package org.sistcoop.producto.models.jpa.entities;
 
+import java.math.BigDecimal;
+
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.NamedQueries;
 import org.hibernate.annotations.NamedQuery;
+import org.sistcoop.producto.models.enums.TipoCuentaPersonal;
 
 @Entity
 @Table(name = "PRODUCTO_CUENTAPERSONAL")
@@ -24,4 +27,12 @@ public class ProductoCuentaPersonalEntity extends ProductoEntity {
 	public final static String findAll = base + "findAll";
 	public final static String findByTipoPersona = base + "findByTipoPersona";
 
+	
+	private TipoCuentaPersonal tipoCuenta;
+	
+	private BigDecimal montoMinimo;
+    private BigDecimal montoMaximo;
+    
+    private int plazoMinimo;
+    private int plazoMaximo;
 }
